83211e1016ce06d76bd0820467f8aa411448e2e1
William Ahrons <william.ahrons@gmail.com>
2017-03-15 16:46:30 -0300

    Add Project Structure

---
+++ src/main/java/com/solidarize/SolidarizeApplication.java
@@ -0,0 +1,12 @@
+package com.solidarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidarizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidarizeApplication.class, args);
+	}
+}
