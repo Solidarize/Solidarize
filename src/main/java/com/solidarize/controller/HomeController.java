ca6da8239abaa85d5c3d5a0c23f691c9b70c06e4
William Ahrons <william.ahrons@gmail.com>
2017-03-16 21:44:12 -0300

    Change to use all verbs

--- src/main/java/com/solidarize/controller/HomeController.java
+++ src/main/java/com/solidarize/controller/HomeController.java
@@ -9,10 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 @Controller
-@RequestMapping(value = "/", method = GET)
+@RequestMapping(value = "/")
 public class HomeController {
 
     private Facebook facebook;
