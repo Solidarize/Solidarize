46d88f6dda2166b0ad358ee5c051fbd9e000421f
“Luciano <lucianoortizsilva@gmail.com>
2017-05-06 21:19:37 -0300

    #8 delete de evento

--- src/test/java/com/solidarize/service/EventServiceTest.java
+++ src/test/java/com/solidarize/service/EventServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,7 +28,7 @@ public class EventServiceTest {
     @Test
     public void shouldBeAbleToGetEvent() throws Exception {
         int id = 1;
-        Event event = new Event("name", "owner",2);
+        Event event = new Event("name", "owner", 2);
         when(repository.findEventById(eq(id))).thenReturn(event);
         Event response = service.getEventById(id);
         assertEquals(event, response);
@@ -35,9 +36,17 @@ public class EventServiceTest {
 
     @Test
     public void shouldBeAbleToSaveAnEvent() throws Exception {
-        Event event = new Event("name", "owner",2);
+        Event event = new Event("name", "owner", 2);
         when(repository.save(eq(event))).thenReturn(event);
         Event response = service.createEvent(event);
         assertEquals(event, response);
     }
+
+    @Test
+    public void shouldBeAbleToDeletarAnEvent() throws Exception {
+        int id = 1;
+        service.deleteEvent(id);
+        verify(repository).delete(eq(id));
+    }
+
 }
