1d6f1fc28e84954a8c003955c3a043b6488b1b9b
William Ahrons <william.ahrons@gmail.com>
2017-05-20 17:28:10 -0300

    Add redirect from root to listEvent url

--- src/main/java/com/solidarize/controller/EventController.java
+++ src/main/java/com/solidarize/controller/EventController.java
@@ -9,15 +9,22 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 public class EventController {
 
+    public static final String LIST_EVENT_URL = "/listEvent";
+
     @RequestMapping(value = "/detailEvent", method = GET)
-    public ModelAndView createDetailEventPage(ModelAndView modelAndView){
+    public ModelAndView createDetailEventPage(ModelAndView modelAndView) {
         modelAndView.setViewName("event/detailEvent");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/listEvent", method = GET)
-    public ModelAndView createListEventPage(ModelAndView modelAndView){
+    @RequestMapping(value = LIST_EVENT_URL, method = GET)
+    public ModelAndView createListEventPage(ModelAndView modelAndView) {
         modelAndView.setViewName("event/listEvent");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/", method = GET)
+    public String redirectToListEvent() {
+        return "redirect:" + LIST_EVENT_URL;
     }
 }
