cb1da77ef984078a5f61a92582bdd00a6f79034b
William Ahrons <william.ahrons@gmail.com>
2017-05-22 23:04:01 -0300

    Fix funcionality of Registry Event

--- src/main/java/com/solidarize/controller/EventManagementController.java
+++ src/main/java/com/solidarize/controller/EventManagementController.java
@@ -1,6 +1,5 @@
 package com.solidarize.controller;
 
-import org.springframework.social.facebook.api.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ public class EventManagementController {
     @GetMapping
     public ModelAndView buildManagementEventView(ModelAndView modelAndView) {
         modelAndView.setViewName("event/registryEvent");
-        modelAndView.addObject(new Event());
         return modelAndView;
     }
 }
